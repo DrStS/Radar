0098bb6d5829663de7678fff7807684376700744
Stefan Sicklinger <mail@sicklinger.com>
2019-09-01 14:42:52 +0200

    verified

--- radarTargetGenerationAndDetection.m
+++ radarTargetGenerationAndDetection.m
@@ -1,7 +1,9 @@
-clear all
+clear all;
+format long;
 clc;
+close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Radar Specifications 
+%% Radar Specifications
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Frequency of operation = 77GHz
 fc= 77e9;
@@ -16,13 +18,13 @@ dres=1;
 %% User Defined Range and Velocity of target
 % define the target's initial position and velocity. Note : Velocity
 % remains contant
- R=110; %initial distance of the target
- v=-20; %velocity of the target
+R=110; %initial distance of the target
+v=-20e2; %velocity of the target +-> approaching
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% FMCW Waveform Generation
 %Speed of light (m/s)
 c = 3*10^8;
-%Calculate the wavelength (m) 
+%Calculate the wavelength (m)
 lambda = c/fc;
 %Design the FMCW waveform by giving the specs of each of its parameters.
 % Calculate the Bandwidth (B), Chirp Time (Tchirp) and Slope (slope) of the FMCW
@@ -32,65 +34,99 @@ Tchirp=5.5*2*Rmax/c; %should be at least 5 to 6 times the round trip time (s)
 slope=B/Tchirp; %(-)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% END FMCW Waveform Generation
-%The number of chirps in one sequence. Its ideal to have 2^ value for the ease of running the FFT for Doppler Estimation. 
-Nd=2;                   % #of doppler cells OR #of sent periods % number of chirps
-%The number of samples on each chirp. 
-Nr=1024;                  %for length of time OR # of range cells
+%The number of chirps in one sequence. Its ideal to have 2^ value for the ease of running the FFT for Doppler Estimation.
+Nd=1;                   % #of doppler cells OR #of sent periods % number of chirps
+%The number of samples on each chirp.
+Nr=2^(25-Nd);                  %for length of time OR # of range cells
+%Nr=2^4;                  %for length of time OR # of range cells
 % Timestamp for running the displacement scenario for every sample on each chirp
 t=linspace(0,Nd*Tchirp,Nr*Nd); %total time for samples
-
+deltaT=t(2)-t(1);
 %Creating the vectors for Tx, Rx and Mix based on the total samples input.
 Tx=zeros(1,length(t)); %transmitted signal
 Rx=zeros(1,length(t)); %received signal
 Mix = zeros(1,length(t)); %beat signal
-
 %Similar vectors for range_covered and time delay.
 r_t=zeros(1,length(t));
 td=zeros(1,length(t));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% some tests
+% https://www.gaussianwaves.com/2014/07/chirp-signal-frequency-sweeping-fft-and-power-spectral-density
+% TxRef = chirp(t,fc,Tchirp,fc+B);
+% Tx = cos((fc+slope*t/2).*t*2*pi);
+% norm(Tx-TxRef)
+n=0;
+for i=1:length(t)
+    tScal=t(i)-(n*Tchirp);    
+    currentFreq=fc+slope*t(i); % This is 1st derivative of (fc+slope*t(i)/2)*t(i)*2*pi
+    Tx(i) = cos((fc+slope*t(i)/2)*t(i)*2*pi);
+    ttilde=tScal-2*(R+v*t(i))/c;
+    if(ttilde>0 || n>0)
+        Rx(i) = cos((fc*ttilde+slope*ttilde*ttilde/2)*2*pi);
+    end
+    if(mod(i,(Nr))==0)
+        n=n+1; %next bin
+        disp(i)
+    end
+end
+Mix=Tx.*Rx;
+windowSize=Nr/2^4;
+[~,~,~,pxx1,fc1,tc1] = spectrogram(Tx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
+ylim([77,77.2]);
+title('Linear Chirp Tx');
+windowSize=Nr/2^4;
+[~,~,~,pxx2,fc2,tc2] = spectrogram(Rx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
+title('Linear Chirp Rx');
+ylim([77,78]);
+plot(tc1(pxx1>0),fc1(pxx1>0),'.');
+hold on;
+plot(tc2(pxx2>0),fc2(pxx2>0),'.r');
+legend('Tx','Rx')
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Signal generation and Moving Target simulation
-% Running the radar scenario over the time. 
-for i=1:length(t)         
+% Running the radar scenario over the time.
+
+for i=1:length(t)
     
     
     % *%TODO* :
-    %For each time stamp update the Range of the Target for constant velocity. 
+    %For each time stamp update the Range of the Target for constant velocity.
     range=R+v*t(i);
     % *%TODO* :
     %For each time sample we need update the transmitted and
-    %received signal. 
+    %received signal.
     
     
     Tx(i) = cos((fc*t(i)+slope*t(i)*t(i)/2)*2*pi);
-    tau = (range/c)*2; %trip time
-    Rx(i) = cos((fc* (t(i)-tau) +(slope*(t(i)-tau)*(t(i)-tau))/2)*2*pi);
+    %   tau = (range/c)*2; %trip time
+    %   Rx(i) = cos((fc* (t(i)-tau) +(slope*(t(i)-tau)*(t(i)-tau))/2)*2*pi);
     
     % *%TODO* :
     %Now by mixing the Transmit and Receive generate the beat signal
     %This is done by element wise matrix multiplication of Transmit and
     %Receiver Signal
-    Mix(i) = cos(2*pi*((2*slope*range/c)*t(i)+(2*fc*v/c)*Tchirp));
+    %  Mix(i) = cos(2*pi*((2*slope*range/c)*t(i)+(2*fc*v/c)*Tchirp));
     
 end
-Mix=Tx.*Rx;
+% Mix=Tx.*Rx;
 %% END Signal generation and Moving Target simulation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% RANGE MEASUREMENT
 
 
- % *%TODO* :
+% *%TODO* :
 %reshape the vector into Nr*Nd array. Nr and Nd here would also define the size of
 %Range and Doppler FFT respectively.
 S=reshape(Mix,Nr,Nd);
 
- % *%TODO* :
+% *%TODO* :
 %run the FFT on the beat signal along the range bins dimension (Nr) and
 %normalize.
 
- % *%TODO* :
+% *%TODO* :
 % Take the absolute value of FFT output
 
- % *%TODO* :
+% *%TODO* :
 % Output of FFT is double sided signal, but we are interested in only one side of the spectrum.
 % Hence we throw out half of the samples.
 
@@ -99,10 +135,10 @@ S=reshape(Mix,Nr,Nd);
 figure ('Name','Range from First FFT')
 subplot(2,1,1)
 
- % *%TODO* :
- % plot FFT output 
+% *%TODO* :
+% plot FFT output
 
- 
+
 axis ([0 200 0 1]);
 
 
@@ -144,7 +180,7 @@ figure,surf(doppler_axis,range_axis,RDM);
 %Select the number of Training Cells in both the dimensions.
 
 % *%TODO* :
-%Select the number of Guard Cells in both dimensions around the Cell under 
+%Select the number of Guard Cells in both dimensions around the Cell under
 %test (CUT) for accurate estimation
 
 % *%TODO* :
@@ -167,19 +203,19 @@ noise_level = zeros(1,1);
 %it a value of 1, else equate it to 0.
 
 
-   % Use RDM[x,y] as the matrix from the output of 2D FFT for implementing
-   % CFAR
+% Use RDM[x,y] as the matrix from the output of 2D FFT for implementing
+% CFAR
 
 
 
 
 
 % *%TODO* :
-% The process above will generate a thresholded block, which is smaller 
+% The process above will generate a thresholded block, which is smaller
 %than the Range Doppler Map as the CUT cannot be located at the edges of
 %matrix. Hence,few cells will not be thresholded. To keep the map size same
-% set those values to 0. 
- 
+% set those values to 0.
+
 
 
 
@@ -195,5 +231,4 @@ figure,surf(doppler_axis,range_axis,'replace this with output');
 colorbar;
 
 
- 
- 
+

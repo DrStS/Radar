c9dba6411793fa5b04969bfe6156fd0f7327cc3f
Stefan Sicklinger <mail@sickligner.com>
2019-09-02 16:36:54 +0200

    t

--- radarTargetGenerationAndDetection.m
+++ radarTargetGenerationAndDetection.m
@@ -19,7 +19,7 @@ dres=1;
 % define the target's initial position and velocity. Note : Velocity
 % remains contant
 R=110; %initial distance of the target
-v=-20e3; %velocity of the target - -> approaching deltaFD = positiv
+v=-20; %velocity of the target - -> approaching deltaFD = positiv
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% FMCW Waveform Generation
 %Speed of light (m/s)
@@ -35,10 +35,10 @@ slope=B/Tchirp; %(-)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% END FMCW Waveform Generation
 %The number of chirps in one sequence. Its ideal to have 2^ value for the ease of running the FFT for Doppler Estimation.
-Nd=2;                   % #of doppler cells OR #of sent periods % number of chirps
+Nd=2^6;                   % #of doppler cells OR #of sent periods % number of chirps
 %The number of samples on each chirp.
-Nr=2^(25-Nd);                  %for length of time OR # of range cells
-%Nr=2^2;                  %for length of time OR # of range cells
+Nr=2^(25-Nd); %for length of time OR # of range cells
+Nr=2^12;
 % Timestamp for running the displacement scenario for every sample on each chirp
 t=linspace(0,Nd*Tchirp,Nr*Nd); %total time for samples
 deltaT=t(2)-t(1);
@@ -46,114 +46,111 @@ deltaT=t(2)-t(1);
 Tx=zeros(1,length(t)); %transmitted signal
 Rx=zeros(1,length(t)); %received signal
 Mix = zeros(1,length(t)); %beat signal
-%Similar vectors for range_covered and time delay.
-r_t=zeros(1,length(t));
-td=zeros(1,length(t));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% some tests
+%% Signal generation and Moving Target simulation
+% Running the radar scenario over the time.
 % https://www.gaussianwaves.com/2014/07/chirp-signal-frequency-sweeping-fft-and-power-spectral-density
+% some tests
 % TxRef = chirp(t,fc,Tchirp,fc+B);
 % Tx = cos((fc+slope*t/2).*t*2*pi);
 % norm(Tx-TxRef)
 n=0;
 m=0;
 endFreqTx=fc+B;
-endFreqRx=(c/(c+v))*fc+B;
+deltaDoppler=(c/(c+v))*fc-fc;
+endFreqRx=endFreqTx+deltaDoppler;
+endFreqRx=endFreqTx;
 for i=1:length(t)
     tScal1=t(i)-(n*Tchirp);
     currentFreqTx=fc+slope*tScal1; % This is 1st derivative of Tx=(fc+slope*t(i)/2)*t(i)*2*pi
     if(currentFreqTx>endFreqTx)
         n=n+1; %next bin
         tScal1=t(i)-(n*Tchirp);
-        disp(i)
     end
     Tx(i) = cos((fc+slope*tScal1/2)*tScal1*2*pi);
-    
+    %Rx
     tScal2=t(i)-(m*Tchirp);
     ttilde=tScal2-2*(R+v*t(i))/c;
-    currentFreqRx=fc+slope*tScal2;
-    
+    currentFreqRx=fc+slope*ttilde;  
     if(currentFreqRx>endFreqRx)
-  %      m=m+1; %next bin
-  %      tScal2=t(i)-(m*Tchirp);
+        m=m+1; %next bin
+        tScal2=t(i)-(m*Tchirp);
+        ttilde=tScal2-2*(R+v*t(i))/c;
     end
-    
     if(ttilde>0)
-        Rx(i) = cos((fc*ttilde+slope*ttilde*ttilde/2)*2*pi);
+        Rx(i) = cos((fc+slope*ttilde/2)*ttilde*2*pi);
     end
 end
+% n=0;
+% for i=1:length(t)
+%       r_t(i)=R+v*t(i); % range of the target in terms of its velocity and initial range
+%       ta(i)=2*r_t(i)/c; % delay for received signal
+%       if i>n*Nr && i<=(n+1)*Nr % doing this for D of periods (nt length of pulse)
+%           Tx(i)=sin(2*pi*(fc*t(i)+.5*slope*t(i)^2-slope*t(i)*n*Tchirp)); %transmitted signal
+%           Rx(i)=sin(2*pi*(fc*(t(i)-ta(i))+.5*slope*(t(i)-ta(i))^2-slope*(t(i)-ta(i))*n*Tchirp)); %received signal
+%       else
+%           n=n+1;
+%           Tx(i)=sin(2*pi*(fc*t(i)+.5*slope*t(i)^2-slope*t(i)*n*Tchirp)); %transmitted signal
+%           Rx(i)=sin(2*pi*(fc*(t(i)-ta(i))+.5*slope*(t(i)-ta(i))^2-slope*(t(i)-ta(i))*n*Tchirp)); %received signal
+%       end
+% end
 Mix=Tx.*Rx;
-windowSize=Nr/2^4;
-[~,~,~,pxx1,fc1,tc1] = spectrogram(Tx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
-ylim([77,77.2]);
-title('Linear Chirp Tx');
-windowSize=Nr/2^4;
-[~,~,~,pxx2,fc2,tc2] = spectrogram(Rx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
-title('Linear Chirp Rx');
-ylim([77,78]);
-plot(tc1(pxx1>0),fc1(pxx1>0),'.');
-hold on;
-plot(tc2(pxx2>0),fc2(pxx2>0),'.r');
-legend('Tx','Rx')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Signal generation and Moving Target simulation
-% Running the radar scenario over the time.
-
-for i=1:length(t)
-    
-    
-    % *%TODO* :
-    %For each time stamp update the Range of the Target for constant velocity.
-    range=R+v*t(i);
-    % *%TODO* :
-    %For each time sample we need update the transmitted and
-    %received signal.
-    
-    
-    Tx(i) = cos((fc*t(i)+slope*t(i)*t(i)/2)*2*pi);
-    %   tau = (range/c)*2; %trip time
-    %   Rx(i) = cos((fc* (t(i)-tau) +(slope*(t(i)-tau)*(t(i)-tau))/2)*2*pi);
-    
-    % *%TODO* :
-    %Now by mixing the Transmit and Receive generate the beat signal
-    %This is done by element wise matrix multiplication of Transmit and
-    %Receiver Signal
-    %  Mix(i) = cos(2*pi*((2*slope*range/c)*t(i)+(2*fc*v/c)*Tchirp));
-    
-end
-% Mix=Tx.*Rx;
+% windowSize=Nr/2^4;
+% [~,~,~,pxx1,fc1,tc1] = spectrogram(Tx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
+% ylim([77,77.2]);
+% title('Linear Chirp Tx');
+% windowSize=Nr/2^4;
+% [~,~,~,pxx2,fc2,tc2] = spectrogram(Rx,windowSize,ceil(windowSize*0.7),windowSize,1/deltaT,'yaxis','MinThreshold',-80);
+% title('Linear Chirp Rx');
+% ylim([77,78]);
+% x1=tc1(pxx1>0);
+% y1=fc1(pxx1>0);
+% plot(x1,y1,'.');
+% hold on;
+% x2=tc2(pxx2>0);
+% y2=fc2(pxx2>0);
+% plot(x2,y2,'.r');
+% legend('Tx','Rx');
+% disp(['================================================================']);
+% disp(['VERIFICATION']);
+% disp(['================================================================']);
+% disp(['Start freq TX ref: ' num2str(fc) ' (Hz), ' num2str(fc/1e9) ' (GHz)']);
+% disp(['Start freq TX: ' num2str(y1(1)) ' (Hz), ' num2str(y1(1)/1e9) ' (GHz) , error (%): ' num2str(((y1(1)-fc)/fc)*1e2)]);
+% disp(['End freq TX ref: ' num2str(endFreqTx) ' (Hz), ' num2str(endFreqTx/1e9) ' (GHz)']);
+% disp(['================================================================']);
+% startFreqRxRef=(c/(c+v))*fc;
+% disp(['Start freq RX ref: ' num2str(startFreqRxRef) ' (Hz), ' num2str(startFreqRxRef/1e9)  ' (GHz)']);
+% startFreqRx=y2(1);
+% disp(['Start freq RX: ' num2str(y2(1)) ' (Hz), ' num2str(startFreqRx/1e9) ' (GHz), error (%): ' num2str(((startFreqRxRef-startFreqRx)/startFreqRx)*1e2)]);
+% disp(['End freq RX ref: ' num2str(endFreqRx) ' (Hz), ' num2str(endFreqRx/1e9) ' (GHz)']);
+% disp(['================================================================']);
+% disp(['Doppler freq shift ref: ' num2str(deltaDoppler) ' (Hz), ' num2str(deltaDoppler/1e9) ' (GHz)']);
+% %disp(['Doppler freq shift: ' num2str(y2(1)-y1(2)) ' (Hz), ' num2str((y2(1)-y1(2))/1e9) ' (GHz), error (%): ' num2str((((y2(1)-y1(2))-deltaDoppler)/deltaDoppler)*1e2)]);
 %% END Signal generation and Moving Target simulation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% RANGE MEASUREMENT
 
-
-% *%TODO* :
 %reshape the vector into Nr*Nd array. Nr and Nd here would also define the size of
 %Range and Doppler FFT respectively.
 S=reshape(Mix,Nr,Nd);
-
-% *%TODO* :
 %run the FFT on the beat signal along the range bins dimension (Nr) and
 %normalize.
-
-% *%TODO* :
+Y = fft(S(:,1));
 % Take the absolute value of FFT output
-
-% *%TODO* :
 % Output of FFT is double sided signal, but we are interested in only one side of the spectrum.
 % Hence we throw out half of the samples.
-
-
+P2 = abs(Y/Nr);
+P2 = fftshift(P2);
+P1 = P2(Nr/2+1:end);
 %plotting the range
 figure ('Name','Range from First FFT')
 subplot(2,1,1)
-
-% *%TODO* :
 % plot FFT output
-
-
-axis ([0 200 0 1]);
-
+dist = linspace(0,(Nr/2)-1,Nr/2);
+plot(dist,P1);
+xlabel('Distance (m)');
+ylabel('|P1(f)|')
+axis ([0 Rmax 0 1]);
 
 
 %% RANGE DOPPLER RESPONSE
@@ -162,12 +159,26 @@ axis ([0 200 0 1]);
 % map.You will implement CFAR on the generated RDM
 
 
+m1=reshape(Mix,Nr,Nd); %generating matrix ---> each row showing range info for one period AND each column showing number of periods
+[My,Ny]=size(m1');
+win=hamming(Ny);
+m2=conj(m1).*(win*ones(1,My)); %taking conjugate and applying window for sidelobe reduction (in time domain)
+Win=fft(hamming(My),Nd);
+M2=(fft(m2,2*Nr)); %First FFT for range information
+M3=fftshift(fft(M2',2*Nd)); %Second FFT for doppler information
+[My,Ny]=size(M3);
+doppler=linspace(-Nd,Nd,My);
+range=linspace(-Nr,Nr,Ny);
+figure;contour(range,doppler,abs(M3));grid on
+xlabel('Range')
+ylabel('Doppler')
+figure;mesh(range,doppler,abs(M3))
+xlabel('Range')
+ylabel('Doppler')
+
 % Range Doppler Map Generation.
-
-% The output of the 2D FFT is an image that has reponse in the range and
-% doppler FFT bins. So, it is important to convert the axis from bin sizes
-% to range and doppler based on their Max values.
-
+% This code is buggy therefore; I needed to reimpelemnt 
+%%%%%%%%%%%%%%%%
 Mix=reshape(Mix,[Nr,Nd]);
 
 % 2D FFT using the FFT size for both dimensions.
@@ -185,6 +196,22 @@ doppler_axis = linspace(-100,100,Nd);
 range_axis = linspace(-200,200,Nr/2)*((Nr/2)/400);
 figure,surf(doppler_axis,range_axis,RDM);
 
+%%%%%%%%%%%%%
+
+Mix=reshape(Mix,Nr,Nd);
+% 2D FFT using the FFT size for both dimensions.
+P2=(fft(Mix,Nr)); %First FFT for range information
+P2=fftshift(fft(P2',Nd)); %Second FFT for doppler information
+P1 = P2(:,Nr/2+1:end);
+sig_fft2 = fft2(Mix,Nr,Nd);
+RDM = abs(P1);
+%RDM = 10*log10(P2) ;
+dist = linspace(0,(Nr/2)-1,Nr/2);
+doppler=linspace(-Nd,Nd,Nd);
+figure();
+surf(dist,doppler,RDM);
+%use the surf function to plot the output of 2DFFT and to show axis in both
+
 %% CFAR implementation
 
 %Slide Window through the complete Range Doppler Map
@@ -201,7 +228,7 @@ figure,surf(doppler_axis,range_axis,RDM);
 
 % *%TODO* :
 %Create a vector to store noise_level for each iteration on training cells
-noise_level = zeros(1,1);
+mwnoise_level = zeros(1,1);
 
 
 % *%TODO* :
